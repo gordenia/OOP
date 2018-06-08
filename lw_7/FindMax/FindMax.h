b61be2d50d1efdb4b94ea6146a0d695dec402bd4
gordenia <gordenia@inbox.ru>
2018-06-08 23:27:42 +0300

    добавлена lw7

---
+++ lw_7/FindMax/FindMax.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include <functional>
+
+template <typename T, typename Less = std::less<T>>
+bool FindMaxEx(std::vector<T> const &arr, T &maxValue, Less const &less = Less())
+{
+	if (arr.empty())
+	{
+		return false;
+	}
+
+	size_t n = arr.size();
+	const T *max = &arr[0];
+	for (size_t i = 0; i < n; ++i)
+	{
+		if (less(*max, arr[i]))
+		{
+				max = &arr[i];
+		}
+	}
+
+	maxValue = *max;
+	return true;
+}
+
